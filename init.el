bd27c8ef0dd833aa34c6edc6b579ef1d33216ab3
Yi Ming <ofseed@foxmail.com>
2022-10-22 02:28:09 +0800

    Enable base org modules

--- init.el
+++ init.el
@@ -153,7 +153,9 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +pretty               ; organize your plain life in plain text
+            +pandoc
+            +roam2)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
