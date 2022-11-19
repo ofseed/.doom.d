723c0838b200a9c4ce583131904c800ffe00a655
Yi Ming <ofseed@foxmail.com>
2022-11-20 03:06:14 +0800

    Fix indent

--- config.el
+++ config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Yi Ming"
-      user-mail-address "ofseed@foxmail.com")
+     user-mail-address "ofseed@foxmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
